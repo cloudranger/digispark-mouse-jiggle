32c2c2846b3ed8843c41f169ae4c9f2d9ab3c0b4
cloudranger1872 <35261145+cloudranger1872@users.noreply.github.com>
2022-02-06 12:34:03 +0100

    Create mouse-jiggle-digispark.c

---
+++ mouse-jiggle-digispark.c
@@ -0,0 +1,34 @@
+#include <DigiMouse.h>
+
+/*
+ *  NAME: Digispark (ATTiny85) Mouse Jiggler
+ *  DESC: Keep your computer awake and unlocked by jiggling the mouse
+ *  VERSION: 1.0
+ *  DATE: 2022-02-05 
+ *  AUTHOR: cloudranger.ch
+ */
+
+// Max delay that can be used in delay() is 16383 - 16 seconds
+
+int sleep = 15000; // Sleep time between mouse jiggles
+int led_lit = 100; // Keep the led lit for this many milliseconds
+
+//int led = 0; // Model B
+int led = 1; // Model A or Pro
+
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+  DigiMouse.begin(); 
+}
+
+void loop()
+{
+  delay(sleep);
+  digitalWrite(led, HIGH);   // turn the LED on
+  DigiMouse.moveX(1);
+  DigiMouse.moveX(-1);
+  delay(led_lit);
+  digitalWrite(led, LOW);   // turn the LED off
+}
